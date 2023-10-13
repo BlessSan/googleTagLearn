548e0c9842ebb55d2884eb5ce8add1cf64d72c1c
blesssan <blessius.septian@gmail.com>
2023-10-13 23:09:17 +0800

    fix lint

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,6 @@ import "./App.css";
 import { useEffect } from "react";
 import TagManager from "react-gtm-module";
 
-function inIframe() {
-  try {
-    return window.self !== window.top;
-  } catch (e) {
-    return true;
-  }
-}
-
 function App() {
   useEffect(() => {
     TagManager.dataLayer({
