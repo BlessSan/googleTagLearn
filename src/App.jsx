20bc9bbdeb39d77ba36fe9bc9b2a87ecb58f4df4
blesssan <blessius.septian@gmail.com>
2023-10-13 22:24:52 +0800

    fix import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import useEffect from "react";
+import { useEffect } from "react";
 import TagManager from "react-gtm-module";
 
 function inIframe() {
